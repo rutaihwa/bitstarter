991e0ca1c37ed232aabacc7cb37076ab5cbeac38
Frank Rutaihwa <rutaihwa@gmail.com>
2013-07-09 10:32:23 +0300

    Updated again web.js

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var reader = fs.readFile('/index.html', function (err, data))
+var reader;
+var test;
+fs.readFile('index.html', function (err, data)
 {
-    if (err) throw err;
-    console.log(data);
+    if (err)
+    {
+	throw err;
+	console.log("Could not read\n");
+    }
+    //console.log (data);
+    test = data.write('utf-8');
+    reader = test.toString();
 });
+
 
 app.get('/', function(request, response) {
   response.send(reader);
